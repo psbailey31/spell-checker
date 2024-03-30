fc6639c18b11409b6fe7f46a4dc57531203c5b26
Paul Bailey <psbailey31@gmail.com>
2024-03-30 00:22:02 +0000

    ghpages

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import AWS from 'aws-sdk';
 function App() {
   return (
     <div className="App">
-        <p>ID:</p> 
+        <p>ID:</p> {process.env.REACT_APP_AWS_ACCESS_KEY_ID}
         
     </div>
   );
